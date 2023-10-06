395ce45d1e1d6a37edf46e51ab9d859ca6edb04d
usama <usamamuhammad231@gmail.com>
2023-10-06 21:43:03 +0500

    bank website complete

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,20 @@
+import { quotes } from "../assets"
+
+const FeedbackCard = ({content, name, img, title}) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="quotes" className="w-[42px] h-[27px] object-contain"/>
+      <p className="text-white my-10 font-poppins font-normal text-[18px] leading-[32px]">{content}</p>
+
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-12 h-12 rounded-full"/>
+        <div className="flex flex-col ml-4">
+          <h4 className="text-white font-poppins font-semibold text-[20px] leading-[32px]">{name}</h4>
+          <p className="text-dimWhite font-poppins font-normal text-[16px] leading-[24px]">{title}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackCard
